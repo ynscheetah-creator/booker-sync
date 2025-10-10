ba1b7c3823b819d8b0bb5ec667ce6bbb8dca9ee7
ynscheetah-creator <ynscheetah@gmail.com>
2025-10-10 14:41:55 +0300

    Update enrich_from_goodreads.py

--- enrich_from_goodreads.py
+++ enrich_from_goodreads.py
@@ -1,5 +1,8 @@
-# enrich_from_goodreads.py
-from notion_sync import run_once
+import sys
+from goodreads_scraper import fetch_goodreads
+import json
 
 if __name__ == "__main__":
-    run_once()
+    url = sys.argv[1] if len(sys.argv) > 1 else "https://www.goodreads.com/book/show/13038020-i-bulma-i-darehanesi"
+    data = fetch_goodreads(url)
+    print(json.dumps(data, ensure_ascii=False, indent=2))
